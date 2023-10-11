3918526266a3335ed7bfb351f1e968b8e84fcb87
Efflerek <piotr.effler@gmail.com>
2023-10-11 15:18:23 +0100

    onsubmit1 onsubmit

--- src/js/script.js
+++ src/js/script.js
@@ -224,43 +224,14 @@ function validateForm() {
  return false; // Zapobiegaj faktycznemu wysłaniu formularza
 }
 
-document.getElementById("consultation").addEventListener("submit", function (event) {
-  event.preventDefault(); // Prevent the default form submission
-
-  // Get form data
-  const formData = new FormData(this);
-
-  // Send the data to getconsultation.php using AJAX
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", "getconsultation.php", true);
-
-  // Set up the callback function to handle the response (if needed)
-  xhr.onload = function () {
-      // Handle the response here (e.g., display a thank you message)
-      if (xhr.status === 200) {
-          // Successful request
-          console.log("Form submitted successfully.");
-          // You can add code to display a success message to the user if needed.
-      } else {
-          // Request failed
-          console.log("Form submission failed.");
-          // You can add code to handle errors here.
-      }
-  };
-
-  xhr.send(formData);
-});
-
   // Callback function for reCAPTCHA
   function onSubmit(token) {
     document.getElementById("form").submit();
   }
 
-  function onSubmit(token) {
+  function onSubmit1(token) {
     document.getElementById("consultation").submit();
   }
-
-});
 
 document.getElementById("consultation").addEventListener("submit", function (event) {
   event.preventDefault(); // Prevent the default form submission
@@ -287,4 +258,5 @@ document.getElementById("consultation").addEventListener("submit", function (eve
   };
 
   xhr.send(formData);
+});
 });
