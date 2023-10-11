de9212667b1898df92c35c32cc63eb4fd62f9f74
Efflerek <piotr.effler@gmail.com>
2023-10-11 13:18:24 +0100

    add files script.js - try to fix translation

---
+++ src/js/script-old.js
@@ -0,0 +1,192 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Cookie popup
+    if (document.cookie.indexOf('cookiePolicyAccepted=true') === -1) {
+      // Create the cookie popup
+      const isIcelandicVersion = window.location.pathname.endsWith('-is.html');
+      const cookiePopup = document.createElement('div');
+      cookiePopup.id = 'cookie-popup';
+  
+      if (isIcelandicVersion) {
+        // Icelandic version
+        cookiePopup.innerHTML = `
+          <p>Vafrakökurstefna</p>
+          <p>Þessi vefsíða notar vafrakökur til að auka upplifun þína. Með því að halda áfram samþykkir þú stefnu okkar um vafrakökur.
+            <br><a href="cookie-policy-ems-is.html" class="link">LINK &#10148;</a>
+          </p>
+          <button class="cookie-popup__button">OK</button>
+        `;
+      } else {
+        // English or other version
+        cookiePopup.innerHTML = `
+          <p>Cookie Policy</p>
+          <p>This website uses cookies to enhance your experience. By continuing, you agree to our cookie policy.
+            <br><a href="cookie-policy-ems.html" class="link">LINK &#10148;</a>
+          </p>
+          <button class="cookie-popup__button">OK</button>
+        `;
+      }
+  
+      document.body.appendChild(cookiePopup);
+  
+      // Handle user's click on "OK" button
+      const cookieButton = document.querySelector('#cookie-popup button');
+      cookieButton.addEventListener('click', function () {
+        // Set the cookie to indicate acceptance
+        document.cookie = 'cookiePolicyAccepted=true; path=/';
+        cookiePopup.style.display = 'none'; // Hide the popup
+      });
+    }
+  // Clipboard copy function
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text)
+      .then(() => {
+        alert('Copied to clipboard: ' + text);
+      })
+      .catch((err) => {
+        console.error('Error copying to clipboard: ', err);
+      });
+  };
+
+  const tabcards = document.querySelectorAll('.tabcard');
+  tabcards.forEach((tabcard) => {
+    tabcard.addEventListener('click', function () {
+      const tabContent = tabcard.querySelector('.tab-content span');
+      const textToCopy = tabContent.textContent;
+      copyToClipboard(textToCopy);
+    });
+  });
+
+  // Modal functionality
+  const openModalBtns = document.querySelectorAll('.sign-up.free-consultation');
+  const modal = document.getElementById("modalform");
+  const closeModal = document.getElementsByClassName("close")[0];
+
+  openModalBtns.forEach((openModalBtn) => {
+    openModalBtn.addEventListener('click', function () {
+      modal.style.display = "flex";
+    });
+  });
+
+  closeModal.addEventListener('click', function () {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener('click', function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+
+  // Mobile menu toggle
+  function toggleMenu() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav-menu").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
+
+  const menuBar = document.querySelector('.menu-bar');
+  menuBar.addEventListener('click', toggleMenu);
+
+  // Language switcher
+  const switcher = document.getElementById('language-toggle');
+  switcher.addEventListener('change', function () {
+    const currentPageName = window.location.pathname.split('/').pop();
+    let newPageName;
+
+    if (currentPageName.endsWith('-is.html')) {
+      newPageName = currentPageName.replace('-is.html', '.html');
+    } else {
+      newPageName = currentPageName.replace('.html', '-is.html');
+    }
+
+    setTimeout(function () {
+      const newURL = window.location.origin + window.location.pathname.replace(currentPageName, newPageName);
+      window.location.href = newURL;
+    }, 200);
+  });
+
+  // Form submit with reCAPTCHA
+  document.getElementById("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    recaptchaCallback();
+  });
+
+  function recaptchaCallback() {
+    document.getElementById("form").submit();
+  }
+
+  // Model Viewer
+  const modelViewers = document.querySelectorAll('model-viewer');
+  modelViewers.forEach(function (modelViewer) {
+    modelViewer.addEventListener('load', function () {
+      console.log('Model has been loaded!');
+    });
+
+    modelViewer.addEventListener('error', function (event) {
+      console.error('An error occurred during model loading:', event);
+    });
+  });
+
+  // Form submission
+  const contactForm = document.getElementById('contact-form');
+  contactForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const formData = new FormData(contactForm);
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', contactForm.getAttribute('action'), true);
+
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        console.log(xhr.responseText);
+      } else {
+        console.error('An error occurred:', xhr.statusText);
+      }
+    };
+
+    xhr.onerror = function () {
+      console.error('Network error occurred');
+    };
+
+    xhr.send(formData);
+  });
+
+  // Max number of words in articles
+  const articles = document.querySelectorAll('.article-body');
+  const maxWords = 25;
+
+  articles.forEach((article) => {
+    const paragraph = article.querySelector('p');
+    if (paragraph) {
+      const words = paragraph.textContent.trim().split(' ');
+      if (words.length > maxWords) {
+        const truncatedText = words.slice(0, maxWords).join(' ') + '...';
+        paragraph.textContent = truncatedText;
+      }
+    }
+  });
+
+  // Form validation
+  function validateForm() {
+    const name = document.forms["form"]["name"].value;
+    const email = document.forms["form"]["email"].value;
+    const message = document.forms["form"]["message"].value;
+
+    if (name === "" || email === "" || message === "") {
+      alert("Please fill in all required fields.");
+      return false;
+    }
+
+    const formMessage = document.getElementById("formMessage");
+    formMessage.style.display = "block";
+    return false;
+  }
+
+  document.getElementById("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+  });
+
+  // Callback function for reCAPTCHA
+  function onSubmit(token) {
+    document.getElementById("form").submit();
+  }
+});
--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,48 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Clipboard copy function
-  const copyToClipboard = (text) => {
-    navigator.clipboard.writeText(text)
-      .then(() => {
-        alert('Copied to clipboard: ' + text);
-      })
-      .catch((err) => {
-        console.error('Error copying to clipboard: ', err);
-      });
-  };
-
+     // Pobierz wszystkie tabcardy
   const tabcards = document.querySelectorAll('.tabcard');
-  tabcards.forEach((tabcard) => {
+
+  // Iteruj przez każdy tabcard
+  tabcards.forEach(function (tabcard) {
+    // Dodaj obsługę zdarzenia kliknięcia
     tabcard.addEventListener('click', function () {
-      const tabContent = tabcard.querySelector('.tab-content span');
+      // Pobierz treść z elementu <span> wewnątrz tabcard
+      const tabContent = tabcard.querySelector('.tabcard .tab-content span');
       const textToCopy = tabContent.textContent;
-      copyToClipboard(textToCopy);
+
+      // Utwórz schowek (Clipboard API)
+      navigator.clipboard.writeText(textToCopy)
+        .then(function () {
+          // Opcjonalnie: Wyświetl komunikat o skopiowaniu do schowka
+          alert('Skopiowano do schowka: ' + textToCopy);
+        })
+        .catch(function (err) {
+          console.error('Błąd kopiowania do schowka: ', err);
+        });
     });
   });
 
-  // Modal functionality
   const openModalBtns = document.querySelectorAll('.sign-up.free-consultation');
   const modal = document.getElementById("modalform");
   const closeModal = document.getElementsByClassName("close")[0];
 
-  openModalBtns.forEach((openModalBtn) => {
-    openModalBtn.addEventListener('click', function () {
+  openModalBtns.forEach(openModalBtn => {
+    openModalBtn.onclick = function () {
       modal.style.display = "flex";
-    });
+    };
   });
 
-  closeModal.addEventListener('click', function () {
+  closeModal.onclick = function () {
     modal.style.display = "none";
-  });
+  };
 
-  window.addEventListener('click', function (event) {
-    if (event.target === modal) {
+  window.onclick = function (event) {
+    if (event.target == modal) {
       modal.style.display = "none";
     }
-  });
+  };
 
-  // Mobile menu toggle
+
   function toggleMenu() {
     document.getElementById("menu-bar").classList.toggle("change");
     document.getElementById("nav-menu").classList.toggle("change");
@@ -50,110 +52,145 @@ document.addEventListener('DOMContentLoaded', function () {
   const menuBar = document.querySelector('.menu-bar');
   menuBar.addEventListener('click', toggleMenu);
 
-  // Language switcher
-  const switcher = document.getElementById('language-toggle');
-  switcher.addEventListener('change', function () {
-    const currentPageName = window.location.pathname.split('/').pop();
-    let newPageName;
+// SWITCHER JĘZYKOWY //
+const switcher = document.getElementById('language-toggle');
 
-    if (currentPageName.endsWith('-is.html')) {
-      newPageName = currentPageName.replace('-is.html', '.html');
-    } else {
-      newPageName = currentPageName.replace('.html', '-is.html');
-    }
+switcher.addEventListener('change', function () {
+  // Pobierz nazwę aktualnej strony
+  const currentPageName = window.location.pathname.split('/').pop();
+  let newPageName; // Deklarujemy zmienną poza blokami if/else
 
-    setTimeout(function () {
-      const newURL = window.location.origin + window.location.pathname.replace(currentPageName, newPageName);
-      window.location.href = newURL;
-    }, 200);
-  });
-
-  // Form submit with reCAPTCHA
-  document.getElementById("form").addEventListener("submit", function (event) {
-    event.preventDefault();
-    recaptchaCallback();
-  });
-
-  function recaptchaCallback() {
-    document.getElementById("form").submit();
+  // Sprawdź, czy strona jest już w formacie "-is.html"
+ if (currentPageName.endsWith('-is.html')) {
+    // Jeśli strona jest w formacie "-is.html", zmień ją na ".html"
+    newPageName = currentPageName.replace('-is.html', '.html');
+  } else {
+    // W przeciwnym razie, zmień na "-is.html"
+    newPageName = currentPageName.replace('.html', '-is.html');
   }
 
-  // Cookie popup
-  if (document.cookie.indexOf('cookiePolicyAccepted=true') === -1) {
-    const isIcelandicVersion = window.location.pathname.endsWith('-is.html');
-    const cookiePopup = document.createElement('div');
-    cookiePopup.id = 'cookie-popup';
+  // Opóźnij przekierowanie o 200 milisekund (0,2 sekundy)
+  setTimeout(function () {
+    // Buduj nowy URL na podstawie nazw stron
+    const newURL = window.location.origin + window.location.pathname.replace(currentPageName, newPageName);
 
-    if (isIcelandicVersion) {
-      cookiePopup.innerHTML = `
-        <p>Vafrakökurstefna</p>
-        <p>Þessi vefsíða notar vafrakökur til að auka upplifun þína. Með því að halda áfram samþykkir þú stefnu okkar um vafrakökur.
-          <br><a href="cookie-policy-ems-is.html" class="link">LINK &#10148;</a>
-        </p>
-        <button class="cookie-popup__button">OK</button>
-      `;
-    } else {
-      cookiePopup.innerHTML = `
-        <p>Cookie Policy</p>
-        <p>This website uses cookies to enhance your experience. By continuing, you agree to our cookie policy.
-          <br><a href="cookie-policy-ems.html" class="link">LINK &#10148;</a>
-        </p>
-        <button class="cookie-popup__button">OK</button>
-      `;
-    }
+    // Przekieruj użytkownika na nową stronę
+    window.location.href = newURL;
+  }, 200);
+});
 
-    document.body.appendChild(cookiePopup);
 
-    const cookieButton = document.querySelector('#cookie-popup button');
-    cookieButton.addEventListener('click', function () {
-      document.cookie = 'cookiePolicyAccepted=true; path=/';
-      cookiePopup.style.display = 'none';
-    });
+  //COOKIE POPUP //
+
+// Sprawdź, czy użytkownik już zaakceptował ciastko
+if (document.cookie.indexOf('cookiePolicyAccepted=true') === -1) {
+  // Ciastko nie jest jeszcze ustawione, utwórz pop-up
+
+  const isIcelandicVersion = window.location.pathname.endsWith('-is.html');
+  const cookiePopup = document.createElement('div');
+  cookiePopup.id = 'cookie-popup';
+
+  if (isIcelandicVersion) {
+    // Wersja islandzka
+    cookiePopup.innerHTML = `
+      <p>Vafrakökurstefna</p>
+      <p>Þessi vefsíða notar vafrakökur til að auka upplifun þína. Með því að halda áfram samþykkir þú stefnu okkar um vafrakökur.
+        <br><a href="cookie-policy-ems-is.html" class="link">LINK &#10148;</a>
+      </p>
+      <button class="cookie-popup__button">OK</button>
+    `;
+  } else {
+    // Wersja angielska lub inna wersja
+    cookiePopup.innerHTML = `
+      <p>Cookie Policy</p>
+      <p>This website uses cookies to enhance your experience. By continuing, you agree to our cookie policy.
+        <br><a href="cookie-policy-ems.html" class="link">LINK &#10148;</a>
+      </p>
+      <button class="cookie-popup__button">OK</button>
+    `;
   }
 
-  // Model Viewer
+  document.body.appendChild(cookiePopup);
+
+  // Obsługa kliknięcia przycisku "OK"
+  const cookieButton = document.querySelector('#cookie-popup button');
+  cookieButton.addEventListener('click', function () {
+    // Ustaw ciastko jako zaakceptowane
+    document.cookie = 'cookiePolicyAccepted=true; path=/';
+
+    // Ukryj pop-up
+    cookiePopup.style.display = 'none';
+  });
+}
+
+  // MODEL 3D
+  // Find all occurrences of the Model Viewer component on the page
   const modelViewers = document.querySelectorAll('model-viewer');
+
+  // Iterate through each Model Viewer component
   modelViewers.forEach(function (modelViewer) {
+    // Check if the component is loaded
     modelViewer.addEventListener('load', function () {
+      // Perform any actions after the model is loaded
       console.log('Model has been loaded!');
     });
 
+    // Check if an error occurred during model loading
     modelViewer.addEventListener('error', function (event) {
+      // Display an error message
       console.error('An error occurred during model loading:', event);
     });
   });
 
-  // Form submission
+
+
+  // SEND FORM
   const contactForm = document.getElementById('contact-form');
+
   contactForm.addEventListener('submit', function (e) {
     e.preventDefault();
+
     const formData = new FormData(contactForm);
     const xhr = new XMLHttpRequest();
+
     xhr.open('POST', contactForm.getAttribute('action'), true);
 
     xhr.onload = function () {
       if (xhr.status === 200) {
-        console.log(xhr.responseText);
+        // Handle success (display a thank you message, for example)
+        console.log(xhr.responseText); // Log the response from the server
       } else {
+        // Handle error
         console.error('An error occurred:', xhr.statusText);
       }
     };
 
     xhr.onerror = function () {
+      // Handle network error
       console.error('Network error occurred');
     };
 
     xhr.send(formData);
   });
 
-  // Max number of words in articles
+  // Max number of words
+  // Get all elements with the class "article"
   const articles = document.querySelectorAll('.article-body');
+
+  // Define the maximum number of words to display
   const maxWords = 25;
 
-  articles.forEach((article) => {
+  // Loop through each article
+  articles.forEach(article => {
+    // Find the <p> element within the article
     const paragraph = article.querySelector('p');
+
+    // Check if the paragraph exists
     if (paragraph) {
+      // Split the text into words
       const words = paragraph.textContent.trim().split(' ');
+
+      // Limit the number of words if it exceeds the maximum
       if (words.length > maxWords) {
         const truncatedText = words.slice(0, maxWords).join(' ') + '...';
         paragraph.textContent = truncatedText;
@@ -161,28 +198,20 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
-  // Form validation
   function validateForm() {
-    const name = document.forms["form"]["name"].value;
-    const email = document.forms["form"]["email"].value;
-    const message = document.forms["form"]["message"].value;
+    const name = document.forms["myForm"]["name"].value;
+    const email = document.forms["myForm"]["email"].value;
+    const message = document.forms["myForm"]["message"].value;
 
-    if (name === "" || email === "" || message === "") {
-      alert("Please fill in all required fields.");
-      return false;
+    if (name == "" || email == "" || message == "") {
+        alert("Please fill in all required fields.");
+        return false;
     }
 
+    // Wyświetl komunikat po pomyślnym wysłaniu
     const formMessage = document.getElementById("formMessage");
     formMessage.style.display = "block";
-    return false;
-  }
 
-  document.getElementById("form").addEventListener("submit", function (event) {
-    event.preventDefault();
-  });
-
-  // Callback function for reCAPTCHA
-  function onSubmit(token) {
-    document.getElementById("form").submit();
-  }
+    return false; // Zapobiegaj faktycznemu wysłaniu formularza
+}
 });
