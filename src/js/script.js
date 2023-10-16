1c3257396ffcdeb6491a9442f11f1939749677f0
Efflerek <piotr.effler@gmail.com>
2023-10-16 11:57:18 +0100

    comment openmodal and form2

--- src/js/script.js
+++ src/js/script.js
@@ -111,7 +111,7 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     }
   });
-  const openModalBtns = document.querySelectorAll('.sign-up.free-consultation');
+ /* const openModalBtns = document.querySelectorAll('.sign-up.free-consultation');
   const modal = document.getElementById('modalform');
   const closeModal = modal.querySelector('.close');
   const form2 = document.getElementById('form2'); // Pobierz formularz "form2"
@@ -136,13 +136,13 @@ document.addEventListener('DOMContentLoaded', function () {
     if (event.target == modal) {
       modal.style.display = 'none';
     }
-  };
+  }; */
   
-  // Obsługa formularza 2 (id="form2")
+  /* Obsługa formularza 2 (id="form2")
   form2.addEventListener('submit', function (e) {
     e.preventDefault();
     onSubmitForm('form2', 'getconsultation.php');
-  });
+  });*/
 
  // Obsługa formularza 1 (id="form")
  document.getElementById('form').addEventListener('submit', function(e) {
